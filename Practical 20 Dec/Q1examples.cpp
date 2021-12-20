2db8985376a87dce9dae44ed96e23d29649a5be3
Manav Raghuwanshi <manav2511@gmail.com>
2021-12-20 15:48:54 +0530

    Improve readability and fix typos and indentation

--- Practical 20 Dec/Q1examples.cpp
+++ Practical 20 Dec/Q1examples.cpp
@@ -1,48 +1,55 @@
 #include <iostream>
+
 #define PI 3.1415926536 //Defined Constant
 
 using namespace std;
 
 int main()
 {
-    /* EXAMPLE 1 */
-    cout<<"Hello World"; //This line displays a message "Hello World" onto the screen 
-    cout<<"This is a C++ class."; //This line displays another message onto the screen
+    /* 
+      EXAMPLE 1 
+    */
+    cout << "Hello World"; //This line displays a message "Hello World" onto the screen 
+    cout << "This is a C++ class."; //This line displays another message onto the screen
 
-
-    /* EXAMPLE 2 */
+    /* 
+      EXAMPLE 2 
+    */
     // Definition 
     int num1;
     int num2;
     int sum;
+
     // Getting Inputs 
-    cout<<"Enter the first nmber: ";
-    cin>>num1;
-    cout<<"Enter the second nmber: ";
-    cin>>num2;
+    cout << "Enter the first nmber: ";
+    cin >> num1;
+    cout <<" Enter the second nmber: ";
+    cin >> num2;
+
     // Calculation and storing result 
     sum = num1 + num2;
     // Display output 
-    cout<<"The sum is: "<<sum;
+    cout << "The sum is: " << sum;
 
-    /* EXAMLE 3 */
+    /* 
+      EXAMPLE 3 
+    */
     // String Constants
-    cout<<"";                   //Empty String
-    cout<<"h";                  //Single length string
-    cout<<"Hello World\n";      //String followed by new line
-    cout<<"'Good' Morning";     //'Good' Morning
-    cout<<"\"Good\" Morning";   //"Good" Morning
-
+    cout << "";                   //Empty String
+    cout << "h";                  //Single length string
+    cout << "Hello World\n";      //String followed by new line
+    cout << "'Good' Morning";     //'Good' Morning
+    cout << "\"Good\" Morning";   //"Good" Morning
 
     /*
-    EXAMPLE 4
-    Three ways to use constants.
+      EXAMPLE 4
+      Three ways to use constants.
     */
-   const double pi = 3.1415926536;
+    const double pi = 3.1415926536;
 
-   cout<<"Defined constant PI: "<<PI<<endl;
-   cout<<"Memory constant pi: "<<pi<<endl;
-   cout<<"Literal constant: "<<3.1415926536<<endl;
+    cout << "Defined constant PI: " << PI << endl;
+    cout << "Memory constant pi: " << pi << endl;
+    cout << "Literal constant: " << 3.1415926536 << endl;
 
-   return 0;
+    return 0;
 }
--- Practical 20 Dec/Q2areaAndCir.cpp
+++ Practical 20 Dec/Q2areaAndCir.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(){
     int radius;
-    cout<<"Enter the RADIUS of the circle: ";
-    cin>>radius;
+    cout << "Enter the RADIUS of the circle: ";
+    cin >> radius;
 
     float pi = (22.0/7);
-    cout<<"Area = "<<pi*radius*radius<<"\nCircumference = "<<2*pi*radius;
+    cout << "Area = " << pi*radius*radius << "\nCircumference = " << 2*pi*radius;
     return 0;
 }
