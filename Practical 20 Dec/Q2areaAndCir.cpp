c5b4bb89496512fb9c0481338a7fc7047b3cc3d6
Om Gupta <86357151+AumGupta@users.noreply.github.com>
2021-12-20 15:37:01 +0530

    Add files via upload
    Practical 20th December 2021

---
+++ Practical 20 Dec/Q2areaAndCir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int radius;
+    cout<<"Enter the RADIUS of the circle: ";
+    cin>>radius;
+
+    float pi = (22.0/7);
+    cout<<"Area = "<<pi*radius*radius<<"\nCircumference = "<<2*pi*radius;
+    return 0;
+}
---
+++ Practical 20 Dec/Q3constants.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#define VOWEL_A "a"
+#define VOWEL_E "e"
+#define VOWEL_I "i"
+#define VOWEL_O "o"
+#define VOWEL_U "u"
+using namespace std;
+
+int main(){
+    const int even1 = 0;
+    const int even2 = 2;
+    const int even3 = 4;
+    const int even4 = 6;
+    const int even5 = 8;
+    cout<<VOWEL_A<<" "<<VOWEL_E<<" "<<VOWEL_I<<" "<<VOWEL_O<<" "<<VOWEL_U<<endl;
+    cout<<even1<<" "<<even2<<" "<<even3<<" "<<even4<<" "<<even5<<endl;
+    cout<<1<<" "<<3<<" "<<5<<" "<<7<<" "<<9;
+    return 0;
+}
