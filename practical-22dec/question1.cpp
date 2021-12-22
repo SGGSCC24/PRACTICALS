4d20af5bc1193fee8e7b3f74c2f976fd6d202792
OmVaish <96373058+OmVaish@users.noreply.github.com>
2021-12-22 14:08:39 +0530

    Create question1.cpp
     Write a program to print the rightmost digit of an integer input by the user.

---
+++ practical-22dec/question1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+      int number , lastdigit ;
+      cout<<"WRITE ANY NUMBER :   \n";
+      cin>>number ;
+      lastdigit = number%10 ;
+      cout<<"SO THE LAST DIGIT OF NUMBER IS "<<lastdigit ;
+      return 0;
+}
